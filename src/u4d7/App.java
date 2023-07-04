ebd3189c2e756007af081d169284e77e493c2efa
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-07-04 12:03:00 +0200

    Adds Iterators

--- src/u4d7/App.java
+++ src/u4d7/App.java
@@ -2,118 +2,147 @@ package u4d7;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
-import u4d7.entities.Student;
+import java.util.Iterator;
 
 public class App {
 
 	public static void main(String[] args) {
 
 		// *************************** ARRAY LIST **************************
-		Student aldo = new Student("Aldo", "Baglio", 20);
-		Student aldo2 = new Student("Aldo", "Baglio", 20);
-		Student giovanni = new Student("Giovanni", "Storti", 30);
-		Student giacomo = new Student("Giacomo", "Poretti", 40);
-
-		// DA ARRAY A LISTA
-		Student[] studentsArray = { aldo, giovanni, giacomo };
-
-		List<Student> lista = new ArrayList<>(Arrays.asList(studentsArray)); // UPCASTING
-
-		// DA LISTA AD ARRAY
-		// Student[] studentsArray2 = (Student[]) lista.toArray();
-
-		System.out.println("La lista ha " + lista.size() + " elementi");
-		lista.add(aldo);
-		lista.add(giovanni);
-		lista.add(giacomo);
-
-		System.out.println("La lista ha " + lista.size() + " elementi");
-
-
-		lista.add(0, new Student("Ajeje", "Brazorf", 50));
-
-		for (Student student : lista) {
-			System.out.println(student);
-		}
-
-		try {
-			System.out.println("--------------- GET --------------------");
-			Student found = lista.get(1);
-			System.out.println("Il secondo elemento della lista è: " + found);
-			System.out.println("Il suo indice è: " + lista.indexOf(aldo));
-		} catch (IndexOutOfBoundsException e) {
-			System.err.println("Elemento non trovato");
-		}
-
-		System.out.println("--------------- CONTAINS --------------------");
-
-		if (lista.contains(aldo2)) { // <-- .contains() internamente utilizza .equals()
-			System.out.println("La lista contiene Aldo");
-		} else {
-			System.err.println("La lista non contiene Aldo");
-		}
-
-		// lista.remove(0);
-		lista.remove(giacomo);
-
-		System.out.println("--------------- REMOVE --------------------");
-
-		for (Student student : lista) {
-			System.out.println(student);
-		}
-
-		System.out.println("La lista è vuota? " + lista.isEmpty());
-		lista.clear();
-
-		System.out.println("--------------- CLEAR --------------------");
-
-		for (Student student : lista) {
-			System.out.println(student);
-		}
-
-		System.out.println("La lista è vuota? " + lista.isEmpty());
-		
-
-		System.out.println("--------------- ADD ALL --------------------");
-		lista.addAll(new ArrayList<>(Arrays.asList(studentsArray)));
-
-		for (Student student : lista) {
-			System.out.println(student);
-		}
-
-		System.out.println("La lista è vuota? " + lista.isEmpty());
-
-		System.out.println("--------------- REMOVE ALL --------------------");
-		lista.add(new Student("Ajeje", "Brazorf", 50));
-		lista.removeAll(new ArrayList<>(Arrays.asList(studentsArray)));
+//		Student aldo = new Student("Aldo", "Baglio", 20);
+//		Student aldo2 = new Student("Aldo", "Baglio", 20);
+//		Student giovanni = new Student("Giovanni", "Storti", 30);
+//		Student giacomo = new Student("Giacomo", "Poretti", 40);
+//
+//		// DA ARRAY A LISTA
+//		Student[] studentsArray = { aldo, giovanni, giacomo };
+//
+//		List<Student> lista = new ArrayList<>(Arrays.asList(studentsArray)); // UPCASTING
+//
+//		// DA LISTA AD ARRAY
+//		// Student[] studentsArray2 = (Student[]) lista.toArray();
+//
+//		System.out.println("La lista ha " + lista.size() + " elementi");
+//		lista.add(aldo);
+//		lista.add(giovanni);
+//		lista.add(giacomo);
+//
+//		System.out.println("La lista ha " + lista.size() + " elementi");
+//
+//
+//		lista.add(0, new Student("Ajeje", "Brazorf", 50));
+//
+//		for (Student student : lista) {
+//			System.out.println(student);
+//		}
+//
+//		try {
+//			System.out.println("--------------- GET --------------------");
+//			Student found = lista.get(1);
+//			System.out.println("Il secondo elemento della lista è: " + found);
+//			System.out.println("Il suo indice è: " + lista.indexOf(aldo));
+//		} catch (IndexOutOfBoundsException e) {
+//			System.err.println("Elemento non trovato");
+//		}
+//
+//		System.out.println("--------------- CONTAINS --------------------");
+//
+//		if (lista.contains(aldo2)) { // <-- .contains() internamente utilizza .equals()
+//			System.out.println("La lista contiene Aldo");
+//		} else {
+//			System.err.println("La lista non contiene Aldo");
+//		}
+//
+//		// lista.remove(0);
+//		lista.remove(giacomo);
+//
+//		System.out.println("--------------- REMOVE --------------------");
+//
+//		for (Student student : lista) {
+//			System.out.println(student);
+//		}
+//
+//		System.out.println("La lista è vuota? " + lista.isEmpty());
+//		lista.clear();
+//
+//		System.out.println("--------------- CLEAR --------------------");
+//
+//		for (Student student : lista) {
+//			System.out.println(student);
+//		}
+//
+//		System.out.println("La lista è vuota? " + lista.isEmpty());
+//		
+//
+//		System.out.println("--------------- ADD ALL --------------------");
+//		lista.addAll(new ArrayList<>(Arrays.asList(studentsArray)));
+//
+//		for (Student student : lista) {
+//			System.out.println(student);
+//		}
+//
+//		System.out.println("La lista è vuota? " + lista.isEmpty());
+//
+//		System.out.println("--------------- REMOVE ALL --------------------");
+//		lista.add(new Student("Ajeje", "Brazorf", 50));
+//		lista.removeAll(new ArrayList<>(Arrays.asList(studentsArray)));
 		// A partire dall'array ho creato una lista di 3 studenti da rimuovere da
 		// "lista"
-
-		for (Student student : lista) {
-			System.out.println(student);
-		}
-
-		System.out.println("La lista è vuota? " + lista.isEmpty());
+//
+//		for (Student student : lista) {
+//			System.out.println(student);
+//		}
+//
+//		System.out.println("La lista è vuota? " + lista.isEmpty());
 
 		// *********************** LINKED LIST ****************************
-
-		LinkedList<Student> linkedStudents = new LinkedList<>();
+//
+//		LinkedList<Student> linkedStudents = new LinkedList<>();
 		// Qua non sto usando l'UPCASTING, il che significa che potrò usare i metodi
 		// specifici di LL
 		// e non solo quelli comuni a tutte le List. Di contro però non potrò sostituire
 		// cambiando un'
 		// unica riga di codice una LL con un ArrayList ad es.
-		linkedStudents.push(giacomo);
-		linkedStudents.pop();
+//		linkedStudents.push(giacomo);
+//		linkedStudents.pop();
 
 		// ************************** LISTE E TIPI PRIMITIVI *****************
-		List<Integer> interi = new ArrayList<>(); // Integer è la WRAPPER CLASS per gli interi
-		// Nelle liste non posso salvare tipi primitivi ma solo oggetti
-		interi.add(2);
-		interi.add(10);
+//		List<Integer> interi = new ArrayList<>(); // Integer è la WRAPPER CLASS per gli interi
+//		// Nelle liste non posso salvare tipi primitivi ma solo oggetti
+//		interi.add(2);
+//		interi.add(10);
+
+		// ************************* ITERATORS ******************************
+		ArrayList<String> lettere = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+
+//		for (String lettera : lettere) {
+//			if (lettera.equals("a")) {
+//				lettere.remove(lettera); 
+		// Java lancia un'eccezione ConcurrentModificationException nel caso
+		// io provassi a modificare la lista (rimuovendo elementi) durante l'iterazione
+//			}
+//			System.out.println(lettera);
+//		}
+
+//		lettere.forEach(lettera -> {
+//			if (lettera.equals("a")) {
+//				lettere.remove(lettera);
+//			}
+//			System.out.println(lettera);
+//		});
+		Iterator<String> i = lettere.iterator();
+		while (i.hasNext()) {
+			String current = i.next(); // "a"
+			if (current.equals("b")) {
+				i.remove();
+				// N.B. non vado a fare lettere.remove(), bensì devo usare il metodo remove
+				// dell'iteratore
+			}
+		}
+
+		for (String string : lettere) {
+			System.out.println(string);
+		}
 
 	}
 
